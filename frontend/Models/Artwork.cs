6c7ac0e4450e0095ca25b3eda240003ac859c3e3
Alessandro Mamolo <71407691+TurboWrestler250@users.noreply.github.com>
2026-05-21 11:56:46 +0200

    update frontend

---
+++ frontend/Models/Artwork.cs
@@ -0,0 +1,27 @@
+﻿namespace frontend.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Artwork
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(100, MinimumLength = 1)]
+    public string Title { get; set; } = default!;
+    [Required]
+    [StringLength(100, MinimumLength = 1)]
+    public string Author { get; set; } = default!;
+    [Required]
+    [StringLength(100, MinimumLength = 1)]
+    public int Year { get; set; }
+    [Required]
+    [StringLength(10000, MinimumLength = 1)]
+    public string Description { get; set; } = default!;
+    [Required]
+    [StringLength(100, MinimumLength = 1)]
+    public string Technique { get; set; } = default!;
+    [Required]
+    [StringLength(1000, MinimumLength = 1)]
+    public string ImageUrl { get; set; } = default!;
+    public int? ExhibitionId { get; set; } = null;
+}
---
+++ frontend/Models/Exhibition.cs
@@ -0,0 +1,12 @@
+﻿namespace frontend.Models;
+
+public class Exhibition
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public DateOnly StartDate { get; set; }
+    public DateOnly FinishDate { get; set; }
+    public string ImageUrl { get; set; } = default!;
+    public string Status { get; set; } = default!;
+}
