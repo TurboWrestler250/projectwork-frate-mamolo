69550c9063c46f41e996423e6550a163b90f422c
DavideFrate <davide.frate.2001@gmail.com>
2026-05-21 17:25:03 +0200

    created details.razor

--- frontend/Services/ExhibitionsService.cs
+++ frontend/Services/ExhibitionsService.cs
@@ -11,21 +11,21 @@ public class ExhibitionsService : IExhibitionsService
             new Exhibition
             {
                 Id = 1,
-                Title = "Exhibition 1",
-                Description = "Description of exhibition 1",
-                StartDate = new DateOnly(2024, 1, 1),
-                FinishDate = new DateOnly(2024, 1, 31),
-                ImageUrl = "https://www.deltaradio.it/resizer/480/-1/true/2025_08_07/ScottiPardi-750x430-1754583136884.jpg--gerry_scotti_compie_69_anni__una_vita_tra_tv__politica_e_successi_senza_tempo.jpg?1754583136913",
+                Title = "Palazzo Attems Petzenstein",
+                Description = "Ultimato nella sua veste rococò nel 1750, oggi ospita mostre temporanee e la Pinacoteca dei Musei Provinciali-ERPAC FVG di Gorizia.",
+                StartDate = new DateOnly(2023, 1, 13),
+                FinishDate = new DateOnly(2026, 5, 21),
+                ImageUrl = "https://backoffice2-comuni.regione.fvg.it/media/files/erpac_musei_gorizia/previews/72840120_1169518193238578_1749242460564881408_o.jpg.640x480_q85_crop.jpg.webp",
                 Status = "Active"
             },
             new Exhibition
             {
                 Id = 2,
-                Title = "Exhibition 2",
-                Description = "Description of exhibition 2",
-                StartDate = new DateOnly(2024, 2, 1),
-                FinishDate = new DateOnly(2024, 2, 28),
-                ImageUrl = "https://www.deltaradio.it/resizer/480/-1/true/2025_08_07/ScottiPardi-750x430-1754583136884.jpg--gerry_scotti_compie_69_anni__una_vita_tra_tv__politica_e_successi_senza_tempo.jpg?1754583136913",
+                Title = "Fototeca",
+                Description = "Migliaia di stampe d’epoca, diapositive, cartoline e negativi, collegati alle vicende locali, a cui si aggiungono le vedute di Gorizia prima e dopo la...",
+                StartDate = new DateOnly(2023, 1, 9),
+                FinishDate = new DateOnly(2026, 5, 21),
+                ImageUrl = "https://backoffice2-comuni.regione.fvg.it/media/files/erpac_musei_gorizia/previews/Gorizia_incrocio_Corso_Verdi_via_Diaz_prima_della_cos_x7lMJcW.jpg.640x480_q85_crop.jpg.webp",
                 Status = "Upcoming"
             }
         });
