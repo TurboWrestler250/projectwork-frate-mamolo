6bba3f777620763c313c82055293b3259d3385de
DavideFrate <davide.frate.2001@gmail.com>
2026-06-03 14:07:41 +0200

    added comments for describing the API's needed

--- frontend/Services/ArtworksService.cs
+++ frontend/Services/ArtworksService.cs
@@ -4,48 +4,54 @@ using frontend.Models;
 
 public class ArtworksService : IArtworksService
 {
+    private readonly HttpClient _http;
+    //public ArtworksService(HttpClient http) => _http = http;
+
     private static readonly List<Artwork> list =
-        [
-            new Artwork
-            {
-                Id = 1,
-                Title = "Persistenza della memoria",
-                Author = "Gerry Scotty",
-                Year = 67,
-                Description = """
-                We're no strangers to love
-                You know the rules and so do I
-                A full commitment's what I'm thinking of
-                You wouldn't get this from any other guy
-                I just wanna tell you how I'm feeling
-                Gotta make you understand
-                Never gonna give you up
-                Never gonna let you down
-                Never gonna run around and desert you
-                Never gonna make you cry
-                Never gonna say goodbye
-                Never gonna tell a lie and hurt you
-                We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game, and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you
-                """,
-                Technique = "Olio su tela",
-                ImageUrl = "https://img.bgstatic.com/multiLang/web/44528784f7bc9c0f6cb1ad0e4cc23f5d.jpg"
-            },
-            new Artwork
-            {
-                Id= 2,
-                Title ="Skibidi toilet",
-                Author = "Thomas Turbato",
-                Description = "Skibidi toilet è un fenomeno virale che ha conquistato internet con la sua combinazione di musica orecchiabile e coreografie stravaganti. Il video originale, pubblicato su YouTube, mostra persone che ballano in modo bizzarro mentre indossano costumi da bagno e si muovono in modo sincronizzato. La canzone, con il suo ritmo contagioso, ha ispirato milioni di persone a creare i propri video di danza Skibidi, rendendo il fenomeno un successo globale. La popolarità del Skibidi toilet ha dimostrato come la creatività e l'umorismo possano unire le persone attraverso i social media, creando una comunità globale di fan che condividono la loro passione per questa stravagante tendenza.",
-                Year = 1690,
-                Technique = "Aglio su pasta",
-                ImageUrl = "https://m.media-amazon.com/images/M/MV5BMzgzMzY2MmMtMWNkNy00ZjVkLWIxOWUtZDJjODNmY2IyOWFiXkEyXkFqcGc@._V1_QL75_UX190_CR0,28,190,281_.jpg"
-            }
-        ];
+    [
+        new Artwork
+        {
+            Id = 1,
+            Title = "Persistenza della memoria",
+            Author = "Gerry Scotty",
+            Year = 67,
+            Description = """
+            We're no strangers to love
+            You know the rules and so do I
+            A full commitment's what I'm thinking of
+            You wouldn't get this from any other guy
+            I just wanna tell you how I'm feeling
+            Gotta make you understand
+            Never gonna give you up
+            Never gonna let you down
+            Never gonna run around and desert you
+            Never gonna make you cry
+            Never gonna say goodbye
+            Never gonna tell a lie and hurt you
+            We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game, and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you
+            """,
+            Technique = "Olio su tela",
+            ImageUrl = "https://img.bgstatic.com/multiLang/web/44528784f7bc9c0f6cb1ad0e4cc23f5d.jpg"
+        },
+        new Artwork
+        {
+            Id= 2,
+            Title ="Skibidi toilet",
+            Author = "Thomas Turbato",
+            Description = "Skibidi toilet è un fenomeno virale che ha conquistato internet con la sua combinazione di musica orecchiabile e coreografie stravaganti. Il video originale, pubblicato su YouTube, mostra persone che ballano in modo bizzarro mentre indossano costumi da bagno e si muovono in modo sincronizzato. La canzone, con il suo ritmo contagioso, ha ispirato milioni di persone a creare i propri video di danza Skibidi, rendendo il fenomeno un successo globale. La popolarità del Skibidi toilet ha dimostrato come la creatività e l'umorismo possano unire le persone attraverso i social media, creando una comunità globale di fan che condividono la loro passione per questa stravagante tendenza.",
+            Year = 1690,
+            Technique = "Aglio su pasta",
+            ImageUrl = "https://m.media-amazon.com/images/M/MV5BMzgzMzY2MmMtMWNkNy00ZjVkLWIxOWUtZDJjODNmY2IyOWFiXkEyXkFqcGc@._V1_QL75_UX190_CR0,28,190,281_.jpg"
+        }
+    ];
+
+    // GET: api/artworks
     public Task<IEnumerable<Artwork>> GetAllAsync()
     {
         return Task.FromResult<IEnumerable<Artwork>>(list);
     }
 
+    // GET: api/artworks/{id}
     public Task<Artwork?> GetItemByIdAsync(int id)
     {
         Artwork? artwork = list.FirstOrDefault(e => e.Id == id);
@@ -53,6 +59,7 @@ public class ArtworksService : IArtworksService
         return Task.FromResult(artwork);
     }
 
+    // POST: api/artworks
     public Task AddAsync(Artwork artwork)
     {
         try
@@ -67,27 +74,31 @@ public class ArtworksService : IArtworksService
         }
     }
 
+    // PUT: api/artworks/{id}
     public Task UpdateAsync(Artwork artwork)
     {
-        try{
-        Artwork? existingArtwork = list.FirstOrDefault(e => e.Id == artwork.Id);
-        if (existingArtwork != null)
+        try
         {
-            existingArtwork.Title = artwork.Title;
-            existingArtwork.Author = artwork.Author;
-            existingArtwork.Year = artwork.Year;
-            existingArtwork.Description = artwork.Description;
-            existingArtwork.Technique = artwork.Technique;
-            existingArtwork.ImageUrl = artwork.ImageUrl;
-        }
+            Artwork? existingArtwork = list.FirstOrDefault(e => e.Id == artwork.Id);
+            if (existingArtwork != null)
+            {
+                existingArtwork.Title = artwork.Title;
+                existingArtwork.Author = artwork.Author;
+                existingArtwork.Year = artwork.Year;
+                existingArtwork.Description = artwork.Description;
+                existingArtwork.Technique = artwork.Technique;
+                existingArtwork.ImageUrl = artwork.ImageUrl;
+            }
             return Task.CompletedTask;
-        }catch (Exception ex)
+        }
+        catch (Exception ex)
         {
             Console.WriteLine($"Error updating artwork: {ex.Message}");
             throw;
         }
     }
 
+    // DELETE: api/artworks/{id}
     public async Task DeleteAsync(int id)
     {
         try
