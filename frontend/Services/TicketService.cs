935ccf6fd47ad4775a5041158b8ccf038c0bc40e
DavideFrate <davide.frate.2001@gmail.com>
2026-06-04 16:19:58 +0200

    added ticket service

---
+++ frontend/Services/TicketService.cs
@@ -0,0 +1,33 @@
+﻿using frontend.Models;
+
+namespace frontend.Services
+{
+    public class TicketService
+    {
+        private readonly HttpClient _httpClient;
+        public TicketService(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(configuration["ApiBaseUrl"] ?? throw new InvalidOperationException("API base URL is not configured."))
+            };
+        }
+
+        public async Task<Ticket> GetTicketFromIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Ticket>($"api/tickets/{id}");
+        }
+
+        public async Task AddTicketAsync(Ticket ticket)
+        {
+            await _httpClient.PostAsJsonAsync("api/tickets", ticket);
+            return;
+        }
+
+        public async Task AddTicketsAsync(List<Ticket> tickets)
+        {
+            await _httpClient.PostAsJsonAsync("api/tickets/batch", tickets);
+            return;
+        }
+    }
+}
